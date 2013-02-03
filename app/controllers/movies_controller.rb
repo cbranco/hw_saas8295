4a0728d741b666c6721dfac1c915cf8a68d9c7f4
cbranco <cbranco@fake.server>
2013-02-03 17:17:20 +0100

    fix

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,7 +15,7 @@ class MoviesController < ApplicationController
     #@movies = Movie.all
     @sort = params[:sort]
     @ratings = params[:ratings]
-    @ratings_ary = @ratings ? @ratings.keys : @all_ratings
+    @ratings_ary = @ratings ? @ratings.keys : @all_ratings,1
     #@movies = Movie.order(@sort)
     @movies = Movie.where(:rating => @ratings_ary).order(@sort)
   end
